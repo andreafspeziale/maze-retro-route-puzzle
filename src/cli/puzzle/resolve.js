e32cf96d740071d858cb0648ff47add845162f71
andreafspeziale <andreaf.speziale@gmail.com>
2019-12-15 00:07:00 +0100

    Apply sanitizeLineBreaks to resolve command output

--- src/cli/puzzle/resolve.js
+++ src/cli/puzzle/resolve.js
@@ -2,6 +2,7 @@ const RetroPuzzleService = require('../../services/RetroPuzzleService')
 const ResolveCommandValidator = require('../../validators/ResolveCommandValidator')
 const ResolveCommand = require('../../commands/puzzle/ResolveCommand')
 const loggerFactory = require('../../factories/loggerFactory')
+const sanitizer = require('../../utils/sanitizer')
 
 const logger = loggerFactory.build()
 const retroPuzzleService = new RetroPuzzleService({ logger })
@@ -11,5 +12,8 @@ const resolveCommand = new ResolveCommand({ retroPuzzleService, resolveCommandVa
 module.exports = {
   describe: resolveCommand.constructor.description,
   builder: resolveCommand.buildArgsDetailsForBuilder(),
-  handler: async argv => logger.info(await resolveCommand.executeAsync(argv)),
+  handler: async (argv) => {
+    const result = await resolveCommand.executeAsync(argv)
+    logger.info({ path: result.map(room => sanitizer.sanitizeLineBreaks(room)) })
+  },
 }
